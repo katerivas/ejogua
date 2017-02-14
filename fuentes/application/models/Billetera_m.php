d5e9645c1ccb67449a734fbaa288265b041e9163
Ana Rivas <kate.rivas0@gmail.com>
2017-02-14 19:49:36 -0300

    Models con mayusculas

---
+++ fuentes/application/models/Billetera_m.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class billetera_m extends CI_Model {
+	/**
+	 * Modelo para manejo de consulta de saldo
+	 */
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function listarSaldo($numero_tarjeta){
+	  	$this->db->select('saldo');
+	  	$this->db->where('numero_tarjeta', $numero_tarjeta);
+
+	  	$v_consulta = $this->db->get('tarjeta');
+	  	return $v_consulta;
+    }
+
+	public function cargar_saldo($data) {
+      	$numero_tarjeta = $data['numero_tarjeta'];
+      	$saldo_obtenido = $this->obtener_saldo($numero_tarjeta);
+      	if ($saldo_obtenido	-> num_rows() > 0){
+      		$resultado = $saldo_obtenido->row();
+      		$saldo = $resultado -> saldo;
+      		$data['saldo'] = $data['saldo'] + $saldo;
+     			$this->db->where('numero_tarjeta', $data['numero_tarjeta']);
+      		$this->db->update('tarjeta', $data);
+      		echo $this->db->last_query();
+      		die();
+      		return true;
+      	}
+	}
+	function get_tarjetas($id_usuario) {
+	        $data = array();
+	        $this->db->select('*');
+					$this->db->where('id_usuario',$id_usuario);
+					$this->db->where('id_estado', 1);
+	        $query = $this->db->get('tarjeta');
+	        if ($query->num_rows() > 0) {
+	            foreach ($query->result_array() as $row){
+	                    $data[] = $row;
+	                }
+	        }
+	        $query->free_result();
+	        return $data;
+
+	  }
+
+    public function obtener_saldo($numero_tarjeta){
+      	$this->db->select('saldo');
+      	$this->db->where('numero_tarjeta', $numero_tarjeta);
+      	$resultado = $this->db->get('tarjeta');
+      	return $resultado;
+      }
+
+	public function existe($numero_tarjeta){
+      	$this->db->where('numero_tarjeta', $numero_tarjeta);
+      	$query = $this->db->get('tarjeta');
+      	if ($query->num_rows() > 0){
+      		return true;
+      	}else{
+      		 return false;
+      	}
+    }
+}
---
+++ fuentes/application/models/Usuario_m.php
@@ -0,0 +1,104 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+Class Usuario_m extends CI_Model{
+	function login($usuario, $password)
+	{
+		$this-> db -> select('id_usuario, usuario');
+		$this-> db -> from('usuarios');
+		$this-> db -> where('usuario', $usuario);
+		$this-> db -> where('password', md5($password));
+		$this-> db -> limit(1);
+		$query = $this -> db -> get();
+		return $query;
+	}
+
+	public function read_user_information($username) {
+
+		$condition = "usuario =" . "'" . $username . "'";
+		$this->db->select('*');
+		$this->db->from('usuarios');
+		$this->db->where($condition);
+		$this->db->limit(1);
+		$query = $this->db->get();
+
+		if ($query->num_rows() == 1) {
+		return $query->result();
+		} else {
+		return false;
+		}
+	}
+
+	public function agregar_usuario($data){
+		$resultado = $this->db->insert('usuarios', $data);
+		return $resultado;
+	}
+
+	public function ver_datos_usuario($id_usuario){
+		$this->db->select('*');
+		$this->db->where('id_usuario', $id_usuario);
+		$query = $this->db->get('usuarios');
+		return $query->result();
+	}
+//we will use the select function
+	public function obtener_datos_usuario()
+	{
+		$this->db->select('*');
+		$this->db->where('usuario', $usuario_sesion);
+		//data is retrive from this query
+		$query = $this->db->get('usuarios');
+		return $query->result();
+	}
+
+	public function ver_usuarios()
+	{
+		$this->db->select('u.id_usuario,
+											u.usuario,
+											r.detalle as detalle_rol,
+											g.detalle as detalle_grupo,
+											e.detalle as detalle_estado');
+		$this->db->from('usuarios u');
+		$this->db->join('roles r', 'u.id_rol = r.id_rol');
+		$this->db->join('grupos g', 'u.id_grupo = g.id_grupo');
+		$this->db->join('estados e', 'u.id_estado = e.id_estado');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function actualizar($id_usuario,$data){
+		$this->db->where('id_usuario', $id_usuario);
+		$this->db->update('usuarios', $data);
+		return true;
+	}
+
+	public function mostrar_id_usuario_seleccionado($id_usuario){
+		$this->db->select('*');
+		$this->db->from('usuarios');
+		$this->db->where('id_usuario', $id_usuario);
+		$query = $this->db->get();
+		return $query->result();
+
+	}
+
+
+	public function actualizar_usuario($id_usuario,$data){
+		$this->db->where('id_usuario', $id_usuario);
+		$this->db->update('usuarios', $data);
+		return true;
+	}
+
+	public function select_roles(){
+		$this->db->select('*');
+		$resultado = $this->db->get('roles');
+		return $resultado->result();
+
+	}
+
+	// public function ver_datos_por_id($id_estado){
+	// 	$this->db->select('*');
+	// 	$this->db->from('usuarios');
+	// 	$this->db->where('id_estado', $id_estado);
+	// 	$query = $this->db->get();
+	// 	return $query->result();
+	// }
+
+
+}
