fb05cad2c5d09dd0080657862846b61eab3eb6bd
Ana Rivas <kate.rivas0@gmail.com>
2017-02-14 20:11:26 -0300

    Models con mayusculas

---
+++ fuentes/application/models/Reclamos_m.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+Class reclamos_m extends CI_Model{
+
+	public function registro_reclamos($data){
+		$resultado = $this->db->insert('reclamos', $data);
+		return $resultado;
+	}
+}
